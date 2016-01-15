b8f20a777c38679ff14b125de690a987493b72aa
jiffin <jiffinv@gmail.com>
2016-01-15 12:19:52 +0530

    Update happy.c

--- happy.c
+++ happy.c
@@ -4,12 +4,12 @@
 int main()
 {
   int n;
-  char string[]="WISH YOU A HAPPY PONGAL":
+  char string[]={"WISH YOU A HAPPY PONGAL"};
   
   for(n=0;string[n]!='\0';n++)
   {
-    sleep(150);
     printf("%c ",string[n]);
+    sleep(50);
   }
   return 0;
 }
