8d80293a8e1fdf0d9e473fd2bde1c331bcf13364
jiffin <jiffinv@gmail.com>
2016-01-28 19:11:23 +0530

    Update decnum.c

--- decnum.c
+++ decnum.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-  int a,d,n,c=0;
+  int a,d,n,c=1;
   printf("enter the number\n");
   scanf("%d",&a);
   
   a=n;  
-  while(a>0)
+  while(a>1)
   {
-    d=a%2;
     c++;
     a=a/2;
   }
