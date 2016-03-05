41f21716fafc1b5c95faf96458a9dec5786cb338
jiffin <jiffinv@gmail.com>
2016-03-05 10:58:25 +0530

    Update varcount.c

--- varcount.c
+++ varcount.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int a[26]={0},n=0,count[10]={0},i,diff,var_c=0;
+	int n=0,count[10]={0},i,d,c=0;
 	char str[10];
 
 	printf("enter the string\n");
@@ -18,21 +18,21 @@ int main()
 		{
 			if((str[i]<='Z')&&(str[i]>='A'))
 			{
-				diff=str[i]-'A';
+				d=str[i]-'A';
 			}
 			else if((str[i]>='a')&&(str[i]<='z'))
 			{
-				diff=str[i]-'a';
+				d=str[i]-'a';
 			}
-			if(count[diff]==0)
+			if(count[d]==0)
 			{
-				count[diff]=1;
-				var_c++;
+				count[d]=1;
+				c++;
 			}
 		}
 	}
 
-	printf("%d\n",var_c);
+	printf("%d\n",c);
 
 	return 0;
 }
